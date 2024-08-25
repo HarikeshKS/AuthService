f44d55a651529fd271f39637a0ebb71221d5190a
Harikesh Kumar Sharma <sharmaharikeshkumar3@gmail.com>
2024-08-25 15:25:26 +0530

    minor destructure fix

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -3,7 +3,10 @@ const UserService = require("../services/user-service");
 const userService = new UserService();
 const create = async (req, res) => {
     try {
-        const response = await userService.create(req.body);
+        const response = await userService.create({
+            email: req.body.email,
+            password: req.body.password,
+        });
         return res.status(201).json({
             data: response,
             success: true,
