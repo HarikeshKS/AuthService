e29d1bb4608147c966190e47ca755ad8b78da382
Harikesh Kumar Sharma <sharmaharikeshkumar3@gmail.com>
2024-08-26 15:12:18 +0530

    Added middleware to validate request for signin and signup

--- src/routes/v1/user-routes.js
+++ src/routes/v1/user-routes.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const UserController = require("../../controllers/user-controller");
+const { AuthValidators } = require("../../middlewares/index");
 
-router.post("/signup", UserController.create);
-router.post("/signin", UserController.signIn);
+router.post("/signup", AuthValidators.userValidators, UserController.create);
+router.post("/signin", AuthValidators.userValidators, UserController.signIn);
 
 module.exports = router;
