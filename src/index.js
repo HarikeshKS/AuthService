df2aca6ed315055aa56aa6d39acf6e6c18de9a17
Harikesh Kumar Sharma <sharmaharikeshkumar3@gmail.com>
2024-09-09 10:29:24 +0530

    minor testing

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { PORT, DB_SYNC } = require("./config/server-config");
 const apiRoutes = require("./routes/index");
-const db = require("./models/index");
+// const db = require("./models/index");
+// const {User, Role} = require("./models/index");
 
 function setupAnfStartServer() {
     const app = express();
@@ -17,6 +18,11 @@ function setupAnfStartServer() {
         // if (DB_SYNC) {
         //     db.sequelize.sync({ alter: true });
         // }
+
+        // const u1 = await User.findByPk(2);
+        // const r1 = await Role.findByPk(2);
+
+        // u1.addRole(r1);
     });
 }
 
